ddbeb299ddfa21c2353493de4da67dcb2b98dd47
BPB Online <business@bpbonline.com>
2024-03-07 10:01:37 +0530

    By BPB Publications

---
+++ Chapter 06/HelloWorldSwiftUI/HelloWorldSwiftUI/HelloWorldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldSwiftUIApp.swift
+//  HelloWorldSwiftUI
+//
+//  Created by hdutt on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
